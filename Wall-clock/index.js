e16acc64bd72644bb07a849b2e6ae9fb98736e55
Agana-Joseph <josephagana8@gmail.com>
2023-02-08 16:02:22 +0100

    Image-Blure

--- Wall-clock/index.js
+++ Wall-clock/index.js
@@ -1,22 +1,22 @@
-const secondHand = document.querySelector('.second-hand');
+const seconHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-function setDate(){
+function setDate() {
     const now = new Date();
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds)
+    const second = now.getSeconds();
+    const secondDegres = ((second / 60) * 360) + 90;
+    seconHand.style.transform = `rotate(${secondDegres}deg)`;
+    console.log(second);
 
     const min = now.getMinutes();
-    const minDegrees = ((min / 60) * 360) + 90;
-    minHand.style.transform = `rotate(${minDegrees}deg)`;
-    console.log(min)
+    const minDegres = ((min / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minDegres}deg)`;
+    console.log(min);
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 60) * 360) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    console.log(hour)
+    const hourDegres = ((hour / 60) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegres}deg)`;
+    console.log(hour);
 }
 setInterval(setDate, 1000);
